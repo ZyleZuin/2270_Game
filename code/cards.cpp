37fc0ba99ceeb24d104a1c40734fdc27bfca4e7b
Kyle Bruin <Kyle.Bruin@me.com>
2013-04-24 14:05:57 -0600

    Outlined possible header file code

--- code/cards.cpp
+++ code/cards.cpp
@@ -2,9 +2,15 @@
  * cards.cpp
  *
  *  Created on: Apr 24, 2013
- *      Author: Kyle
+ *      Author: Kyle Bruin & Cameron Bigelow
  */
+#include "cards.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
 
+int main (int argc, char* argv[])
+{
 
-
+}
--- code/cards.h
+++ code/cards.h
@@ -2,13 +2,38 @@
  * cards.h
  *
  *  Created on: Apr 24, 2013
- *      Author: Kyle
+ *      Author: Kyle Bruin & Cameron Bigelow
  */
 
 #ifndef CARDS_H_
 #define CARDS_H_
 
+#include <string>
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+class Card;
+class Deck;
+
+class Deck {
+private:
+	vector<Card> cards; // Index is position in the deck
+public:
+	int addCard(char index, string suit); // Add a new card to the deck
+	int removeCard();
+	int getSize(); // Returns size of deck
+	int moveCard(); // Send card to another deck
+};
+
+class Card {
+private:
+	char index;
+	string suit;
+	string color;
+public:
+};
 
 
 #endif /* CARDS_H_ */
